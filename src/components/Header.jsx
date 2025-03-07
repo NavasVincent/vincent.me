baa3ddbfee47332879b3dd9ec93c89ea3316a163
NavasVincent <blue.lens10@gmail.com>
2025-03-08 03:22:16 +0800

    mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -127,7 +127,7 @@ const Header = () => {
                       <FeatherIcon
                         icon={icon}
                         size={20}
-                        fill="#fff"
+                        // fill="#fff"
                         stroke="white"
                         strokeWidth={1}
                       />
